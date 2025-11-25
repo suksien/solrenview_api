cd674ce8775615361da091d46be03fd42bb6ac0d
suksien <hahasuksien@yahoo.com.my>
2025-11-25 22:20:59 +0800

    organization

---
+++ etl_pipeline.py
@@ -0,0 +1,16 @@
+import requests
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+# extract weather data from sftp server into csv file
+def api_config():
+  
+def extract_from_api():
+
+
+# extract electrical data from web api into csv file
+
+if __name__ == "__main__":
+  main()
