2604625e28a0ecdfbce360957cd2cb9c77f7ac41
suksien <hahasuksien@yahoo.com.my>
2025-11-26 21:59:33 +0800

    simulate daily extraction

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -20,7 +20,27 @@ def extract_electrical_data(start_date, end_date):
     df_arr.append(utils.get_inverter_data_arr(response_arr, 1, timestamp))
   
   df = pd.DataFrame().append(df_arr)
-  df.to_csv(f'data_{start_date}_{end_date}.csv')
+  df.to_csv(f'data/extracted/{start_date}_{end_date}.csv')
 
-# load csv file to cloud storage
-extract_electrical_data('2025-10-25', '2025-10-26')
+# load csv file to cloud storage??
+
+# def transform_load_electrical_data():
+
+
+# simulate daily extraction
+from datetime import date, timedelta
+
+d1, d2 = date(2025, 10, 20), date(2025, 10, 21)
+
+start_date_arr = ['2025-10-20']
+end_date_arr = ['2025-10-21']
+
+for i in range(5):
+  d1 += timedelta(days=1)
+  d2 += timedelta(days=1)
+  start_date_arr.append(d1.strftime('%Y-%m-%d'))
+  end_date_arr.append(d2.strftime('%Y-%m-%d'))
+
+for i in range(len(start_date_arr)):
+  start_date, end_date = start_date_arr[i], end_date_arr[i]
+  extract_electrical_data(start_date, end_date)
