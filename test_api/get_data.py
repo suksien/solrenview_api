e7891ad643a743cff001bba219c92aa0cf8cb447
Suk Sien Tie <hahasuksien@yahoo.com.my>
2025-12-19 21:22:45 +0800

    pivoting to backup plan

---
+++ test_api/get_data.py
@@ -0,0 +1,22 @@
+import requests
+import pandas as pd
+from google.cloud import storage
+from datetime import datetime
+
+def get_data(url):
+  out = requests.get(url)
+  data = out.json()
+  items = data['items']
+  df = pd.DataFrame(items)
+  df.drop(columns=['@id', 'eaRegionName', 'floodArea'])
+  return df
+
+def save_dataframe_to_storage(df, bucket_name):
+    csv_string = df.to_csv(index=False)
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    
+    time_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    blob = bucket.blob(f'{time_now}.csv')
+    blob.upload_from_string(csv_string, content_type='text/csv')
