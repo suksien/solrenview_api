d724b2bdfec3e18452573587650b0c17bffd44ca
Suk Sien Tie <hahasuksien@yahoo.com.my>
2026-01-03 14:38:05 +0800

    pipeline draft

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -23,8 +23,10 @@ def extract_electrical_data(start_date, end_date):
     df_arr.append(utils.get_inverter_data_arr(response_arr, id, timestamp))
   
   df = pd.concat(df_arr)
+  return df
   #df.to_csv(f'data/extracted/{start_date}_{end_date}.csv')
-  df.to_csv(f'/Users/suksientie/Desktop/solren_data/{start_date}_{end_date}.csv')
+  #df.to_csv(f'/Users/suksientie/Desktop/solren_data/{start_date}_{end_date}.csv')
+  
 
 # https://stackoverflow.com/questions/40683702/upload-csv-file-to-google-cloud-storage-using-python
 '''
@@ -35,16 +37,37 @@ def load_csv_to_bucket(csv_file, bucket_name = "solrenview_data"):
   blob.upload_from_filename(csv_file)
 '''
 
+def upload_df_to_gcs(df, bucket_name, filepath, filename):
+    """Uploads a pandas DataFrame to a GCS bucket as a CSV."""
+    
+    # 1. Convert DataFrame to a CSV string buffer
+    csv = df.to_csv(index=False)
+    
+    # 2. Initialize GCS client and get the bucket
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(f'{filepath}/{filename}')
+    
+    # 3. Upload the CSV data from the buffer
+    blob.upload_from_string(csv, content_type='text/csv')
+    
+    print(f"DataFrame uploaded to gs://{bucket_name}/{blob}")
+
+
 # simulate daily extraction
 from datetime import date, timedelta
 
 d1 = date(2025, 11, 14)
 d2 = d1 + timedelta(days=1)
 
+ndays = (date.today() - d1).days
+print(f"Downloading {ndays} days of data from {d1} to {date.today()}")
+
+'''
 start_date_arr = [d1.strftime('%Y-%m-%d')]
 end_date_arr = [d2.strftime('%Y-%m-%d')]
 
-for i in range(20):
+for i in range(ndays):
   d1 += timedelta(days=1)
   d2 += timedelta(days=1)
   start_date_arr.append(d1.strftime('%Y-%m-%d'))
@@ -54,3 +77,4 @@ for i in range(len(start_date_arr)):
   start_date, end_date = start_date_arr[i], end_date_arr[i]
   print(start_date)
   extract_electrical_data(start_date, end_date)
+'''
